83d616cab01f44ee0f2294b53c2532d2cab0ea04
Krystian Drożdżyński <krystian@drozdzynski.me>
2018-11-26 14:26:18 +0100

    Fix issue "null is not an object" on Mobile Safari 11.0,

--- ts/tooltip/main.ts
+++ ts/tooltip/main.ts
@@ -64,7 +64,7 @@ export default class Tooltip extends Module {
   }
 
   public hide() {
-    if (this.tooltip.parentElement !== null) {
+    if (this.tooltip.parentElement) {
       document.body.removeChild(this.tooltip)
     }
   }
