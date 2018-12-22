bcb49578343f2e9cc502adc0ede7df1f654346ff
Krystian Drożdżyński <krystian@drozdzynski.me>
2018-12-22 09:49:09 +0100

    Scrollbar better performance and browser search compatibility.

--- ts/scrollbar/events.ts
+++ ts/scrollbar/events.ts
@@ -5,22 +5,13 @@ export default class Events {
   private scrollbar: Scrollbar
   private currentY: number
 
-  private isMac: RegExpMatchArray = navigator.platform.match(/(Mac|iPhone|iPod|iPad)/i)
-
-  private isWheeling: NodeJS.Timer = null
-
-  private watcher: NodeJS.Timer = null
-  private fps: number = 1000 / 24
-
   constructor(scrollbar: Scrollbar) {
     this.scrollbar = scrollbar
 
-    this.scrollbar.el.onwheel = event => this.mouseWheel(event)
-    this.scrollbar.scroll.onwheel = event => this.mouseWheel(event)
     this.scrollbar.bar.onmousedown = event => this.mouseDown(event)
-    this.scrollbar.el.onscroll = event => this.userScrolled(event)
+    this.scrollbar.bar.ontouchstart = event => this.touchStart(event)
 
-    this.scrollbar.el.ontouchstart = event => this.touchStart(event)
+    this.scrollbar.el.onscroll = event => this.userScrolled(event)
 
     document.onmouseup = event => this.mouseUp(event)
     document.ontouchend = event => this.touchEnd(event)
@@ -42,21 +33,6 @@ export default class Events {
     this.scrollbar.move(scrollDistance)
   }
 
-  private mouseWheel(this: Events, event: WheelEvent) {
-    let distance = null
-
-    if (event.deltaY % 120 === 0) {
-      distance = -(event.deltaY / 10);
-    } else {
-      distance = event.deltaY
-    }
-
-    if ((distance > 0 && this.scrollbar.el.scrollTop < (this.scrollbar.scrollHeight - this.scrollbar.height)) || (distance < 0 && this.scrollbar.el.scrollTop > 0)) {
-      event.preventDefault()
-      this.scrollbar.move(distance)
-    }
-  }
-
   private mouseUp(this: Events, event: MouseEvent) {
     document.onmousemove = null
   }
@@ -65,7 +41,7 @@ export default class Events {
     const touch = event.touches[0] || null
     if (event.touches.length === 1 && touch !== null) {
       this.currentY = touch.pageY
-      this.scrollbar.el.ontouchmove = e => this.touchMove(e)
+      this.scrollbar.bar.ontouchmove = e => this.touchMove(e)
     }
   }
 
@@ -74,7 +50,7 @@ export default class Events {
     const touch = event.touches[0] || null
     if (event.touches.length === 1 && touch !== null) {
       const moveDistance = (touch.pageY - this.currentY)
-      const scrollDistance = -moveDistance
+      const scrollDistance = (moveDistance / this.scrollbar.maxPosition) * (this.scrollbar.scrollHeight - this.scrollbar.height)
       this.currentY = touch.pageY
 
       this.scrollbar.move(scrollDistance)
--- ts/scrollbar/main.ts
+++ ts/scrollbar/main.ts
@@ -3,6 +3,7 @@ import Events from './events'
 
 export default class Scrollbar extends Module {
 
+  public wrapper: HTMLElement
   public scroll: HTMLElement
   public bar: HTMLElement
 
@@ -24,18 +25,25 @@ export default class Scrollbar extends Module {
   constructor(el: HTMLElement) {
     super(el)
 
+    this.wrap()
     this.createScroll()
     this.calculateSizes()
+  }
+
+  public wrap() {
+    this.el.classList.add('scrollbar')
+
+    this.wrapper = document.createElement('div');
+    this.wrapper.className = 'scrollbar-wrapper'
+    this.el.parentNode.insertBefore(this.wrapper, this.el);
+    this.wrapper.appendChild(this.el);
   }
 
   public calculateSizes() {
     this.height = this.el.clientHeight
     this.scrollHeight = this.el.scrollHeight
-    this.el.style.overflow = 'hidden'
-    this.el.style.display = 'inline-block'
 
     this.el.style.width = null
-    this.el.style.width = 'calc(100% - ' + this.scroll.offsetWidth + 'px)'
     this.width = this.el.clientWidth
 
     const visibleProportion = this.height / this.scrollHeight
@@ -58,12 +66,6 @@ export default class Scrollbar extends Module {
 
   public move(distance: number) {
     this.el.scrollTop = (distance + this.el.scrollTop)
-
-    this.scroll.className = this.scroll.className.indexOf('scroll-active') > 0 ? this.scroll.className : this.scroll.className + ' scroll-active'
-    clearTimeout(this.scrollClass)
-    this.scrollClass = setTimeout(() => {
-      this.scroll.className = this.scroll.className.replace(/\s*scroll-active\s*/g, '')
-    }, 500)
   }
 
   public setBarPosition() {
@@ -78,6 +80,12 @@ export default class Scrollbar extends Module {
     }
 
     this.bar.style.marginTop = this.position + 'px';
+
+    this.scroll.classList.add('scroll-active')
+    clearTimeout(this.scrollClass)
+    this.scrollClass = setTimeout(() => {
+      this.scroll.classList.remove('scroll-active')
+    }, 500)
   }
 
   public onTick() {
